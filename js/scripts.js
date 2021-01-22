fc8c0c71e941b94b048d9b54c84273953f942f6d
Maggie S <Maggiesaldivia@gmail.com>
2021-01-21 22:17:19 -0800

    add slideToggle for the first four cards and specific CSS stylings and classes for them.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
-  $(".card").click(function() {
-    $(".card.card-front").slideToggle();
-    $(".card.card-back").slideToggle();
+  $(".card.js").click(function() {
+    $(".card.js.card-front").slideToggle();
+    $(".card.js.card-back").slideToggle();
+
+  });
+});
+
+$(document).ready(function() {
+  $(".card.op").click(function() {
+    $(".card.op.card-front").slideToggle();
+    $(".card.op.card-back").slideToggle();
+
+  });
+});
+
+$(document).ready(function() {
+  $(".card.vb").click(function() {
+    $(".card.vb.card-front").slideToggle();
+    $(".card.vb.card-back").slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".card.vn").click(function() {
+    $(".card.vn.card-front").slideToggle();
+    $(".card.vn.card-back").slideToggle();
   });
 });
